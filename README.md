1f55f4c855a2558ff1a59591ef99c2218a568d81
jamie-jessi <jamiejudi@gmail.com>
2017-05-20 01:51:27 -0500

    bare minimum. any key be used for input

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Psychic-Game
+
+initial commit 5/20
+-basic requirements met / can still input letters
---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//variables
+//available choices
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+				'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+//starting variables
+var wins = 0;
+var losses = 0;
+var guesses = 8;
+var guessedLetters = [];
+
+//initial comp choice
+var compChoice = letters[Math.floor(Math.random() * letters.length)];
+
+//function to reset game w/o refreshing page
+var reset = function() {
+    guesses = 8;
+    guessedLetters = [];
+    compChoice = letters[Math.floor(Math.random() * letters.length)];
+    }
+
+//player initiates game with key press
+document.onkeyup = function (event) {
+//changes input to lower case   
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    guessedLetters.push(userGuess);
+
+    if (userGuess === compChoice) {
+    	wins ++;
+        reset();
+    	}
+    else {
+    	guesses--;
+    }
+
+    if (guesses === 0) {
+    	losses++;
+        reset();
+    }
+    
+	document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guessLeft").innerHTML = "Guesses Left: " + guesses;
+    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters.join(', ');
+};
